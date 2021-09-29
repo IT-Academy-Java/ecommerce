08067b9115d449c529846b979356cc8f92722eb2
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-29 09:49:36 +0200

    ADD Brand entity

--- src/main/java/com/competidor/ecommerce/entity/Brand.java
+++ src/main/java/com/competidor/ecommerce/entity/Brand.java
@@ -1,2 +1,39 @@
-package com.competidor.ecommerce.entity;public class Brand {
+package com.competidor.ecommerce.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="brand")
+public class Brand {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name="id")
+  private Integer id_brand;
+
+  private String name;
+
+  public Brand() {}
+
+  public Brand(int id_brand, String name) {
+    this.id_brand = id_brand;
+    this.name = name;
+  }
+
+  public int getId_brand() {
+    return id_brand;
+  }
+
+  public void setId_brand(int id_brand) {
+    this.id_brand = id_brand;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
