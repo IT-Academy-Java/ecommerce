2f63d721add2d334256f8ed02d529a2eb33a7e6c
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-29 09:50:32 +0200

    add brand repository

--- src/main/java/com/competidor/ecommerce/repository/IBrandRepository.java
+++ src/main/java/com/competidor/ecommerce/repository/IBrandRepository.java
@@ -1,2 +1,11 @@
-package com.competidor.ecommerce.repository;public class IBrandRepository {
+package com.competidor.ecommerce.repository;
+
+
+import com.competidor.ecommerce.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBrandRepository extends JpaRepository<Brand, Integer> {
+
 }
