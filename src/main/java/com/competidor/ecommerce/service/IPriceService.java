d2d6bef827e768e9837ff8e8fc2b654b49923ac2
Elena Piaggio <elenapiaggio@gmail.com>
2021-10-03 13:50:46 +0200

    UPDATE IPriceService

--- src/main/java/com/competidor/ecommerce/service/IPriceService.java
+++ src/main/java/com/competidor/ecommerce/service/IPriceService.java
@@ -3,11 +3,16 @@ package com.competidor.ecommerce.service;
 
 import com.competidor.ecommerce.entity.Price;
 
+import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface IPriceService {
+
   public List<Price> getPrices();
   public Optional<Price> getPriceById(int id);
   public List<Price> getPriceByProductId(int id);
+  public Optional<Price> getOnePriceByProductAndBrandAndPriority(Date date, int productId, int brandId) throws ParseException;
+
 }
