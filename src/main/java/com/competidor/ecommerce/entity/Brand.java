fc7e2cd043d1609b88962ca045a62178b2d710cd
Elena Piaggio <elenapiaggio@gmail.com>
2021-10-03 22:35:10 +0200

    ADD @Data annotations in Brand entity

--- src/main/java/com/competidor/ecommerce/entity/Brand.java
+++ src/main/java/com/competidor/ecommerce/entity/Brand.java
@@ -1,8 +1,10 @@
 package com.competidor.ecommerce.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
-
+@Data
 @Entity
 @Table(name="brand")
 public class Brand {
@@ -10,30 +12,8 @@ public class Brand {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name="id")
-  private Integer id_brand;
+  private Integer idBrand;
 
   private String name;
 
-  public Brand() {}
-
-  public Brand(int id_brand, String name) {
-    this.id_brand = id_brand;
-    this.name = name;
-  }
-
-  public int getId_brand() {
-    return id_brand;
-  }
-
-  public void setId_brand(int id_brand) {
-    this.id_brand = id_brand;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
 }
