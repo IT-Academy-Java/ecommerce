108d54fe9bb1cfc422c0ee2da8cf2b27ea37b697
Elena Piaggio <elenapiaggio@gmail.com>
2021-10-03 22:49:33 +0200

    update atributes names

--- src/main/java/com/competidor/ecommerce/entity/Price.java
+++ src/main/java/com/competidor/ecommerce/entity/Price.java
@@ -16,13 +16,23 @@ public class Price {
   @Column(name="id")
   private Integer idPrice;
 
+  @Column(name="start_date")
   private Date startDate;
+
+  @Column(name="end_date")
   private Date endDate;
+
+  @Column(name="price_list")
   private int priceList;
+
+  @Column(name="product_id")
   private int productId;
+
   private int priority;
   private double price;
   private String curr;
+
+  @Column(name="brand_id")
   private Integer brandId;
 
 }
